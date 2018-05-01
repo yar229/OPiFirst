c1becc235a1af7bf98e24b58161a91f4398c9248
YaR <yar229@yandex.ru>
2018-05-02 02:03:57 +0300

    OLED: old single font removed

--- DemoSSD1306/ssd1306_i2c.c
+++ DemoSSD1306/ssd1306_i2c.c
@@ -32,7 +32,6 @@ All text above, and the splash screen below must be included in any redistributi
 #include <wiringPiI2C.h>
 #include <stdint.h>
 #include "fonts.h"
-#include "oled_fonts.h"
 
 #define true 1
 #define false 0
@@ -787,43 +786,43 @@ void ssd1306_drawString(font_info_t *font, unsigned char *str, int x, int y)
 }
 
 // Draw a character
-void _backup_ssd1306_drawChar(int x, int y, unsigned char c, int color, int size)
-{
+//void _backup_ssd1306_drawChar(int x, int y, unsigned char c, int color, int size)
+//{
+//
+//	if ((x >= WIDTH) ||	// Clip right
+//	    (y >= HEIGHT) ||	// Clip bottom
+//	    ((x + 6 * size - 1) < 0) ||	// Clip left
+//	    ((y + 8 * size - 1) < 0))	// Clip top
+//		return;
+//	int i;
+//	int j;
+//	for (i = 0; i < 6; i++) 
+//	{
+//		int line;
+//		if (i == 5)
+//			line = 0x0;
+//		else
+//			line = pgm_read_byte(font + (c * 5) + i);
+//
+//		for (j = 0; j < 8; j++) 
+//		{
+//			if (line & 0x1) 
+//			{
+//				if (size == 1)	// default size
+//					ssd1306_drawPixel(x + i, y + j, color);
+//				else 
+//				{	// big size
+//					ssd1306_fillRect(x + (i * size),
+//							 y + (j * size), size,
+//							 size, color);
+//				}
+//			}
+//			line >>= 1;
+//		}
+//	}
+//}
 
-	if ((x >= WIDTH) ||	// Clip right
-	    (y >= HEIGHT) ||	// Clip bottom
-	    ((x + 6 * size - 1) < 0) ||	// Clip left
-	    ((y + 8 * size - 1) < 0))	// Clip top
-		return;
-	int i;
-	int j;
-	for (i = 0; i < 6; i++) 
-	{
-		int line;
-		if (i == 5)
-			line = 0x0;
-		else
-			line = pgm_read_byte(font + (c * 5) + i);
-
-		for (j = 0; j < 8; j++) 
-		{
-			if (line & 0x1) 
-			{
-				if (size == 1)	// default size
-					ssd1306_drawPixel(x + i, y + j, color);
-				else 
-				{	// big size
-					ssd1306_fillRect(x + (i * size),
-							 y + (j * size), size,
-							 size, color);
-				}
-			}
-			line >>= 1;
-		}
-	}
-}
-
-
+// Draw a character
 uint8_t ssd1306_drawChar(font_info_t *font, uint8_t x, uint8_t y, unsigned char c, int foreground, int background, uint8_t size)
 {
 	uint8_t i, j;
